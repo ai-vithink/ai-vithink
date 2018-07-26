e449475994e814fcb1d7714ff7b974824e5ca9b5
the-lost-explorer <parundekaramey@gmail.com>
2018-07-26 23:21:51 +0530

    Fixed events scroll

--- js/actions.js
+++ js/actions.js
@@ -1,18 +1,27 @@
 $(document).ready(function(){
+
+    //Opening the events tab
     $("#events-button").on("click",function(){
         $("#buttons").animate({top:200});
         $(".foot").animate({top:200});
         $(".events").css("display", "inline-block" );
     });
 
-    $('.card').scroll(function() { 
-        $('.cross').animate({top:$(this).scrollTop()},0.001,"linear");
-    });
+    $(document).on("click", function(){
 
-    $("#events-cross").on("click", function(){
-        $("#buttons").animate({top:0});
-        $(".foot").animate({top:0});
-        $(".events").css("display", "none" );
+        //Closing the events tab
+        if($(event.target).attr("class") != "events card" && $(event.target).parent().attr("class") !="media" && $(event.target).parent().attr("class") !="subtitle is-6" && $(event.target).parent().attr("class") != "card-content" && $(event.target).parent().attr("class") != "events card" && $(event.target).parent().attr("class") != "content"  && $(event.target).parent().attr("class") != "events-card" && $(event.target).parent().attr("class") != "media-content" && $(event.target).parent().attr("class") != "container"  && event.target.id != "events-button"){
+            $("#buttons").animate({top:0});
+            $(".foot").animate({top:0});
+            $(".events").css("display", "none" );
+        }
+
+        //Closing the events section for image. (Because for some reason it doesn't work with the abve if statement #issue)
+        if($(event.target).attr("class") == "image"){
+            $("#buttons").animate({top:0});
+            $(".foot").animate({top:0});
+            $(".events").css("display", "none" );
+        }
     });
 
 
