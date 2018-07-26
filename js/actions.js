84dff20349d94471d0f45cef0006460a12f56858
the-lost-explorer <parundekaramey@gmail.com>
2018-07-26 23:03:46 +0530

    Added cross

--- js/actions.js
+++ js/actions.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
         $(".events").css("display", "inline-block" );
     });
 
-    $(document).on("click", function(){
-        console.log($(event.target).attr("class"));
-        if( $(event.target).attr("class") != "events card" && $(event.target).parent().attr("class") !="media" && $(event.target).parent().attr("class") !="subtitle is-6" && $(event.target).parent().attr("class") != "card-content"&& $(event.target).parent().attr("class") != "events card" && $(event.target).parent().attr("class") != "content"  && $(event.target).parent().attr("class") != "events-card" && $(event.target).parent().attr("class") != "media-content" && $(event.target).parent().attr("class") != "container"  && event.target.id != "events-button"){
-            $("#buttons").animate({top:0});
-            $(".foot").animate({top:0});
-            $(".events").css("display", "none" );
-            flag = 0;
-        }
+    $('.card').scroll(function() { 
+        $('.cross').animate({top:$(this).scrollTop()},0.001,"linear");
+    });
+
+    $("#events-cross").on("click", function(){
+        $("#buttons").animate({top:0});
+        $(".foot").animate({top:0});
+        $(".events").css("display", "none" );
     });
 
 
