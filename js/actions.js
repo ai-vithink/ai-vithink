3771b11842b3a9b563c56a799687ddd27cbf7865
the-lost-explorer <parundekaramey@gmail.com>
2018-07-27 12:00:15 +0530

    Added About Us

--- js/actions.js
+++ js/actions.js
@@ -2,9 +2,27 @@ $(document).ready(function(){
 
     //Opening the events tab
     $("#events-button").on("click",function(){
-        $("#buttons").animate({top:200});
-        $(".foot").animate({top:200});
+        $("#about").css("display","none");
+        $("#buttons").animate({top:560});
+        $(".foot").animate({top:560});
+        $("#main").animate({
+            width: "200px",
+            height: "200px"
+        
+        });
         $(".events").css("display", "inline-block" );
+    });
+
+    $("#about-us").on("click",function(){
+        $(".events").css("display","none");
+        $("#buttons").animate({top:560});
+        $(".foot").animate({top:560});
+        $("#main").animate({
+            width: "200px",
+            height: "200px"
+        
+        });
+        $("#about").css("display", "inline-block" );
     });
 
     $(document).on("click", function(){
@@ -13,14 +31,24 @@ $(document).ready(function(){
         if($(event.target).attr("class") != "events card" && $(event.target).parent().attr("class") !="media" && $(event.target).parent().attr("class") !="subtitle is-6" && $(event.target).parent().attr("class") != "card-content" && $(event.target).parent().attr("class") != "events card" && $(event.target).parent().attr("class") != "content"  && $(event.target).parent().attr("class") != "events-card" && $(event.target).parent().attr("class") != "media-content" && $(event.target).parent().attr("class") != "container"  && event.target.id != "events-button"){
             $("#buttons").animate({top:0});
             $(".foot").animate({top:0});
+            $("#main").animate({
+                width: "300px",
+                height: "300px"
+            });
             $(".events").css("display", "none" );
+            $("#about").css("display", "none" );
         }
 
         //Closing the events section for image. (Because for some reason it doesn't work with the abve if statement #issue)
-        if($(event.target).attr("class") == "image"){
+        if($(event.target).attr("class") == "image" ||$(event.target).attr("class") == "delete"){
             $("#buttons").animate({top:0});
             $(".foot").animate({top:0});
+            $("#main").animate({
+                width: "300px",
+                height: "300px"
+            });
             $(".events").css("display", "none" );
+            $("#about").css("display", "none" );
         }
     });
 
