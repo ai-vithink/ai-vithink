1f1000e4c5f3d19a728150cb58a73d52c74c9726
the-lost-explorer <parundekaramey@gmail.com>
2018-08-01 23:56:18 +0530

    Results

--- js/actions.js
+++ js/actions.js
@@ -59,40 +59,7 @@ $(document).ready(function(){
         }
     });
 
-    /* 
-     * This is to make it clear that what follows below is not my creation. 
-     * I didn't have enough time to code for it. 
-     * It has been picked up from online sources
-     */
-    //Countdown 
-    // Set the date we're counting down to
-    var countDownDate = new Date("Aug 1, 2018 23:59:59").getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-        // Get todays date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Display the result in the element with id="demo"
-        document.getElementById("countdown-append").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-
-        // If the count down is finished, write some text 
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("countdown-append").innerHTML = "Loading!";
-            window.location.href = "https://ai-vithink.github.io";
-        }
-    }, 1000);
-
+    $("#result").on("click",function(){
+        $(".modal").attr("class","is-active");
+    });
 });
