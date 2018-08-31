9e64ef7918b7d6545a4be9440117d9f1d78312dc
the-lost-explorer <parundekaramey@gmail.com>
2018-09-01 01:22:27 +0530

    Added pages

--- js/actions.js
+++ js/actions.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
+    
+    $("#ai-training-tab").on("click",function(){
+        window.location.href = "ai.html";
+    });
+
+    $("#home-tab").on("click",function(){
+        window.location.href = "index.html";
+    });
+
+    $("#about-us-tab").on("click",function(){
+        window.location.href = "board.html";
+    });
+
+    $("#view-srini").on("click",function(){
+        window.location.href = "srini.html";
+    });
+
+    $("#view-ebe").on("click",function(){
+        window.location.href = "ebe.html";
+    });
 
 });
