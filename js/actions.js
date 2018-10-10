c48eeb2c92edfa97710b9f9b04f66359f543b9cc
the-lost-explorer <parundekaramey@gmail.com>
2018-10-11 00:01:22 +0530

    Added several stuff

--- js/actions.js
+++ js/actions.js
@@ -20,9 +20,54 @@ $(document).ready(function(){
         window.location.href = "ebe.html";
     });
 
+    $("#view-susan").on("click",function(){
+        window.location.href = "susan.html";
+    });
+
     $("#events-tab").on("click",function(){
         window.location.href = "events.html";
     });
 
+    $("#mentors-tab").on("click",function(){
+        window.location.href = "mentors.html";
+    });
 
+    $("#experience-tab").on("click",function(){
+        window.location.href = "experience.html";
+    });
+
+    $("#resources-tab").on("click",function(){
+        window.location.href = "resources.html";
+    });
+
+
+    // Read more button (Sorry this is a very shitty way to do it. I just don;t have the time to make the code less repetitive)
+    $(".read-more-1").on('click', function() {
+        document.querySelector('#content-1').style.height= 'auto';
+        this.style.display='none';
+
+        $(".hide-more-1").css("display","inline");
+      });
+
+    $(".hide-more-1").on('click', function() {
+    document.querySelector('#content-1').style.height= '10em';
+    this.style.display='none';
+
+    $(".read-more-1").css("display","inline");
+    });
+
+    $(".read-more-2").on('click', function() {
+        document.querySelector('#content-2').style.height= 'auto';
+        this.style.display='none';
+
+        $(".hide-more-2").css("display","inline");
+      });
+
+    $(".hide-more-2").on('click', function() {
+    document.querySelector('#content-2').style.height= '10em';
+    this.style.display='none';
+
+    $(".read-more-2").css("display","inline");
+    });
+      
 });
